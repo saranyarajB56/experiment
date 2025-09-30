5b04e1f5dfa0bc237eab55bc23c5daa494f06a39
saranyarajB56 <saranyaraj765@gmail.com>
2025-09-30 15:37:53 +0530

    Add files via upload

---
+++ add_digit.py
@@ -0,0 +1,18 @@
+print("          Addition of a digits       ")
+print("        -----------------------      ")
+n = int(input("enter a value : "))
+s = 0             
+while n > 0:
+ b =  n % 10
+ s = s + b
+ n = n // 10
+print(s)
+
+
+
+
+
+
+
+
+
