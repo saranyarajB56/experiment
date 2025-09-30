182bde02ca06c90e0c796727a93c8b31a3c8b3a9
saranyarajB56 <saranyaraj765@gmail.com>
2025-09-30 15:48:32 +0530

    Add files via upload

---
+++ largestof3.py
@@ -0,0 +1,10 @@
+
+a = int(input("Enter first Number : "))
+b = int(input("Enter second Number : "))
+c = int(input("Enter third Number : "))
+if a > b and a > c :
+    print(a)
+elif b > a and b > c :
+    print(b)
+else :
+    print(c)
