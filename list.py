3635f88608434c0d8e31fd24e17aa7b671c12774
saranyarajB56 <saranyaraj765@gmail.com>
2025-09-30 15:41:32 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,22 @@
+# add an element to a list
+a = [1,2,3,4,5] 
+a.append(10)
+print(a)
+
+#  add an element to an index
+b =[1,2,3,4,5,6]
+b.insert(1,"python")
+print(b)
+
+#  remove an element from the list
+c = [1,2,3,4,5,6]
+c.pop()
+print(c)
+
+# remove an element from an index
+d = [1,2,3,4,5]
+d.pop(1)
+print(d)
+
+
+
