680430b432cc3f8a78e5672a1493d536bbf8a802
saranyarajB56 <saranyaraj765@gmail.com>
2025-10-01 11:58:29 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,17 @@
+# lambda function
+
+#   print fullname
+fullname = lambda x,y,z:x+y+z
+print(fullname(" mariam" , "sara" , "johny"))
+
+# squre root of a number
+sqr = lambda a: a**2
+print(sqr(10))
+
+# Average of 5 numbers
+avg = lambda a,b,c,d,e:(a+b+c+d+e)/5
+print(avg(10,20,30,40,50))
+
+# perimeter of a circle
+peri = lambda r:(2*3.14*r)
+print(peri(10))
